d10201b938bf2b55f1f4d6c21567428bd39d3a48
Ayleen <islandordaz@gmail.com>
2019-05-31 02:49:57 -0400

    fixed login function, added some basic styles
    im tired

--- src/index.js
+++ src/index.js
@@ -1,34 +1,74 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
+import axios from 'axios'
 class LogIn extends React.Component {
 
-    submit = e => {
-        e.preventDefault()
-        console.log(e.target.elements.ID.value)
-        var box = { 
-            username:e.target.elements.ID.value,
-            password:e.target.elements.psw.value
+    constructor(props){
+        super(props)
+        this.state={
+            id:'',
+            passwd:''
         }
-        console.log(JSON.stringify(box))
-        fetch('http://localhost:3001/api/login', {method: 'POST', body: JSON.stringify(box)})
-            .then(res => res.json())
-            .then(json => console.log(json))
     }
+
+    changeHandler = (event) =>{
+       this.setState({
+           
+        [event.target.name]: event.target.value
+
+       } )
+    
+    }
+  
+    formHandler = (event) => {
+        event.preventDefault()
+        console.log(this.state)
+        axios.post(`http://localhost:3001/api/login`, this.state)
+        .then(response => {console.log(response.data)})
+        .then(error => {console.log(error)})
+    }
+
+    // submit = e => {
+    //     e.preventDefault()
+    //     console.log(typeof(e.target.elements.psw.value))
+
+    //     //let request = { 
+    //      //   username: e.target.elements.ID.value ,
+    //       //  password: e.target.elements.psw.value  
+    //     //}
+    //     console.log(JSON.stringify(request))
+    //     fetch('http://localhost:3001/api/login', {
+    //         method: 'POST', 
+    //         body: JSON.stringify(e.value)
+    //       })
+    //     .then(res => res.json())
+    //     .then(data => console.log(data))
+    // }
 
     render(){
     return(
-        <div>
-            <h1> Login </h1>
-            <form onSubmit={this.submit}>
+        
+
+            <div className="boxThing">
+                   <h1> Login </h1>
+    
+              <form onSubmit={this.formHandler}>
+
                 <label> NID or knights Email </label>
-                <input name="ID" type="text"></input>
+                <br></br>
+                <input name="id" value={this.state.id} onChange={this.changeHandler} type="text"></input>
+                <br></br>
                 <label> password </label>
-                <input name="psw" type="password" ></input>
-                <button>Submit</button>
+                <br></br>
+                <input name="passwd" value={this.state.passwd} onChange={this.changeHandler} type="password" ></input>
+                <br></br>
+                <button type="submit">Submit</button>
+                <br></br>
             </form>
-        </div>
+            
+            </div>
+        
     )
 }
 }
@@ -51,20 +91,10 @@ class HomePage extends React.Component{
         loading:false
     }
 
-
-    // fetches the data from the frontEnd
-     componentDidMount(){
-         fetch('http://localhost:3001/api', {method: 'GET'})
-             .then(res=>res)
-             .then(res=> console.log(res))
-
-     }
-
-
     render(){
         return(
             <div>
-                <h1>Senior Design Portal</h1>
+                <h1 className="header">Senior Design Portal</h1>
                 {this.state.loggedIn ? <Dashboard /> : <LogIn/>}
             </div>
 
@@ -75,3 +105,9 @@ ReactDOM.render(
         <HomePage />,
     document.getElementById('root')
 )
+//palette
+// Light tan : #F5F0E6 background general
+// bright gold: #EAAB0B headers and buttons
+// dark violet: #312A37 highlight over things idk use sparingly
+// white:#FFFFFF text on dark backgrounds
+// dark grey/black: #282C0B headings, outlines, text light backgrounds
