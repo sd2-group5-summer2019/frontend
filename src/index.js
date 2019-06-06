a8acd960616abb166a6eb72255ee04a7aed98a90
Ayleen <islandordaz@gmail.com>
2019-06-06 17:33:54 -0400

    updates
    deleted code i wasn't using, changed state of login to match backend

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import axios from 'axios';
-
+import { Route, Redirect, Switch } from 'react-router'
 
 class LogIn extends React.Component {
 
     constructor(props){
         super(props)
         this.state={
-            id:'',
-            passwd:''
+            username:'',
+            password:''
         }
     }
-
+    
     changeHandler = (event) =>{
        this.setState({
            
@@ -22,12 +22,20 @@ class LogIn extends React.Component {
        } )
     
     }
+    
+    componentWillUnmount(){
+        this.setState({
+            username:'',
+            password:''
+        })
+    }
   
     formHandler = (event) => {
         event.preventDefault()
         console.log(this.state)
         axios.post(`http://localhost:3001/api/login`, this.state)
-        .then(response => {console.log(response.data)})
+        .then(function(response){
+        })
         .then(error => {console.log(error)})
     }
 
@@ -44,61 +52,40 @@ class LogIn extends React.Component {
 
                 <label> NID or knights Email </label>
                 <br></br>
-                <input name="id" value={this.state.id} onChange={this.changeHandler} type="text"></input>
+                <input name="username" value={this.state.id} onChange={this.changeHandler} type="text"></input>
                 <br></br>
                 <label> password </label>
                 <br></br>
-                <input name="passwd" value={this.state.passwd} onChange={this.changeHandler} type="password" ></input>
+                <input name="password" value={this.state.passwd} onChange={this.changeHandler} type="password" ></input>
                 <br></br>
                 <button type="submit">Submit</button>
                 <br></br>
             </form>
-            
+                     
             </div>
+
+             
+
+           
         
     )
 }
 }
-const Dashboard = () =>{
+
+
+function Survey (){
     return(
         <div>
+            <h1 className="header">Senior Design Portal</h1>
             <p>gonna put a navbar in here somewhere as another component</p>
             <h1>Dashboard</h1>
         </div>
     )
 }
 
-class HomePage extends React.Component{
-    // if user is not logged in shows only the log in page
-    // so the state tracks whether or not user is logged in
-    // it shows if the page is loading while accessing the data that
-    // is sent back from the end
-    state = {
-        loggedIn:false,
-        user: ''
-    }
-
-    render(){
-        return(
-            <div>
-                <h1 className="header">Senior Design Portal</h1>
-                {this.state.loggedIn ? <Dashboard /> : <LogIn/>}
-            </div>
-
-        )
-    }
-}
-
 
  ReactDOM.render(
-        <HomePage />,
+        <LogIn />,
      document.getElementById('root')
  )
 
-
-//palette
-// Light tan : #F5F0E6 background general
-// bright gold: #EAAB0B headers and buttons
-// dark violet: #312A37 highlight over things idk use sparingly
-// white:#FFFFFF text on dark backgrounds
-// dark grey/black: #282C0B headings, outlines, text light backgrounds
