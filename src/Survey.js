72d21193306e43408ab504fbb55e5fef1ef73aa1
Ayleen <islandordaz@gmail.com>
2019-06-09 02:33:19 -0400

    Survey page now sends request
    USING POST NOT GET, BACKEND NEEDS TO CHANGE API.js file for it to work
    -Survey page sends request for a survey, sends (form_id) via post request, then prints the data it receives to the console log,

--- src/Survey.js
+++ src/Survey.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import axios from 'axios';
 
 
 const Question = ({question="Title not available"})=> {
@@ -18,35 +18,78 @@ class Survey extends React.Component{
             {"question": "Survey Question 2"}
         ]
     }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            form_id:'',
+            loading:false,
+            questionList: [],
+            form_retreived:false
+        };
     
-    state = {
-        form_id:'',
-        loading:false,
-        questionList: [],
-        form_retreived:false
+        this.handleChange1 = this.handleChange1.bind(this);
+        this.requestForm = this.requestForm.bind(this);
+        this.changePage = this.changePage.bind(this);
+      }
+    
+    
+    
+
+    handleChange1(event){
+        this.setState({
+            form_id:event.target.value
+        }) 
     }
 
+    requestForm(event){
+        event.preventDefault()
+        this.setState({loading:true})
+        console.log(this.state.form_id)
+        axios.post(`http://localhost:3001/api/getSurvey`, this.state)
+        .then(response => this.changePage(response))
+        .catch(function (error){console.log(error)})
+    }
+
+    changePage(res){
+        if(res.status===200){
+            this.setState({
+                questionList:res.data,
+                loading:false,
+                form_retreived:true
+            })
+            console.log(res.data)
+        }
+        else
+            console.log("Something went wrong")
+    }
+    // additional functions
+    // SubmitForm()
+    // handleChange2 for the answers to the survey questions
+
     render(){
+        //question array is just for testing right now has hardcoded values (see default props above)
         const {questions} = this.props
         const formStatus= this.state.form_retreived
+        
+
         if(!formStatus){
             return(
                 <div>
                 <h1>Request Survey/form (for testing purposes for now)</h1>
                 <p> this isnt gonna be a thing in the final product though</p>
-                <form>
+                <form onSubmit={this.requestForm}>
                     <label>Enter Form Id </label>
-                    <input type="text"/>
-                    <button>Request Form/survey</button>
+                    <input type="text" name="form_id" value={this.state.form_id} onChange={this.handleChange1}></input>
+                    <button type="submit">Request Form/survey</button>
                 </form>
                 </div>
             )
-        }
-        else{
+        } else {
             return(
                 <div>
                     <h1 className="header">Survey1</h1>
-                    <form>
+                    <form >
                         <div>
                         {questions.map((questions, i)=>
                             <Question key={i} question={questions.question} />  
