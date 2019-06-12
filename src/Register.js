42fe06e95d61b009cfdd64fd144c1590c5248fc9
Mahzain Malik <mahzain@MacBook-Air.local>
2019-06-12 18:59:47 -0400

    Minor changes to Registration form (uses radio buttons for types)

--- src/Register.js
+++ src/Register.js
@@ -53,13 +53,25 @@ class Register extends React.Component{
     
     validateForm() {
         
+        let first_name = this.state.first_name;
+        let last_name = this.state.last_name;
         let username = this.state.username
         let password = this.state.password
         let email = this.state.email
-      
+        let type = this.state.type;
         let errors = {};
         let formIsValid = true;
 
+        if (!first_name) {
+        formIsValid = false;
+        errors["first_name"] = "*Please enter your first name.";
+        }
+                       
+        if (!last_name) {
+        formIsValid = false;
+        errors["last_name"] = "*Please enter your last name.";
+        }
+                       
         if (!username) {
             formIsValid = false;
             errors["username"] = "*Please enter your username.";
@@ -74,6 +86,11 @@ class Register extends React.Component{
             formIsValid = false;
             errors["password"] = "*Please enter your password.";
         }
+                
+       if (!type) {
+       formIsValid = false;
+       errors["type"] = "*Please select a user type.";
+       }
 
         this.setState({
                       errors: errors
@@ -91,38 +108,53 @@ class Register extends React.Component{
                     <div className="boxThing" id="register">
                         <h1>Registration</h1>
                     <form method="post"  name="userRegistrationForm"  onSubmit= {this.submitHandler}>
+                            <label>First Name</label>
+                            <br></br>
+                            <div className="errorMsg">{this.state.errors.first_name}</div>
+                            <br></br>
+                            <input type="text" name="first_name" value={this.state.first_name} onChange={this.changeHandler} />
+                            <br></br>
+                    
+                            <label>Last Name</label>
+                            <br></br>
+                            <div className="errorMsg">{this.state.errors.last_name}</div>
+                            <br></br>
+                            <input type="text" name="last_name" value={this.state.last_name} onChange={this.changeHandler} />
+                            <br></br>
                     
                             <label>Username or NID:</label>
                             <br></br>
-                            <input type="text" name="username" value={this.state.username} onChange={this.changeHandler}/>
-                            <br></br>
                             <div className="errorMsg">{this.state.errors.username}</div>
+                            <br></br>
+                            <input type="text" name="username" value={this.state.username} onChange={this.changeHandler}/>
                             <br></br>
                     
                             <label>Email:</label>
                             <br></br>
-                            <input type="text" name="email" value={this.state.email} onChange={this.changeHandler}/>
-                            <br></br>
                             <div className="errorMsg">{this.state.errors.email}</div>
                             <br></br>
-
-                            <label>Password</label>
+                            <input type="text" name="email" value={this.state.email} onChange={this.changeHandler}/>
                             <br></br>
-                            <input type="password" name="password" value={this.state.password} onChange={this.changeHandler} />
+                    
+                            <label>Password</label>
                             <br></br>
                             <div className="errorMsg">{this.state.errors.password}</div>
                             <br></br>
-                            <label>Student or Instructor</label>
+                            <input type="password" name="password" value={this.state.password} onChange={this.changeHandler} />
                             <br></br>
-                            <input type="text" name="type" value={this.state.type} onChange={this.changeHandler} />
+
+                            <label>User Type</label>
                             <br></br>
-                            <label>First Name</label>
+                            <div className="errorMsg">{this.state.errors.type}</div>
                             <br></br>
-                            <input type="text" name="first_name" value={this.state.first_name} onChange={this.changeHandler} />
-                            <br></br>
-                            <label>Last Name</label>
-                            <br></br>
-                            <input type="text" name="last_name" value={this.state.last_name} onChange={this.changeHandler} />
+                            <b>Student</b>
+                            <input type="radio" name="type" value={this.state.type === 'Student'} onChange={this.changeHandler} />
+                            <b>Admin</b>
+                            <input type="radio" name="type" value={this.state.type === 'Admin'} onChange={this.changeHandler} />
+                            <b>Sponsor</b>
+                            <input type="radio" name="type" value={this.state.type === 'Sponsor'} onChange={this.changeHandler} />
+                            <b>Coordinator</b>
+                            <input type="radio" name="type" value={this.state.type === 'Coordinator'} onChange={this.changeHandler} />
                             <br></br>
                             <input type="submit" className="button"  value="Register"/>
                         </form>
