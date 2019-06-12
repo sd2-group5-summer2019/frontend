0c55f1c034c80f844802b0494d76cc14953b376d
Ayleen <islandordaz@gmail.com>
2019-06-11 21:15:01 -0400

    Survey now works fully
    ok i honestly don't remeber everything i did lol I was kind of flying through it, uh survey now generates a form with the data it gets from the backend, and then when user hits submit on form it packages it into a payload and sends it to the backend, right now it's only sending it when you click submit, but we might be able to do something like an autosave, that saves the questions you've already completed, idk let me know if I messed up somewhere

--- src/Survey.js
+++ src/Survey.js
@@ -2,14 +2,6 @@ import React from "react";
 import axios from 'axios';
 
 
-const Question = ({question="Title not available"})=> {
-    return(
-        <section>
-            <h2>{question}</h2>
-            <input name="answer" type="text"/>
-        </section>
-    )
- }
 
 class Survey extends React.Component{
     static defaultProps = {
@@ -23,19 +15,26 @@ class Survey extends React.Component{
         super(props);
         this.state = {
             form_id:'',
+            student_id:'2',
+            token:'testToken',
             loading:false,
-            questionList: [],
-            form_retreived:false
+            title:'',
+            type:'',
+            data: {},
+            results:{},
+            form_retreived:false,
+            form_submitted:false
         };
     
         this.handleChange1 = this.handleChange1.bind(this);
         this.requestForm = this.requestForm.bind(this);
         this.changePage = this.changePage.bind(this);
+        this.questionHandler= this.questionHandler.bind(this);
+        this.formHandler = this.formHandler.bind(this);
+        this.redirectOnSubmit = this.redirectOnSubmit.bind(this);
       }
     
     
-    
-
     handleChange1(event){
         this.setState({
             form_id:event.target.value
@@ -49,29 +48,75 @@ class Survey extends React.Component{
         axios.post(`http://localhost:3001/api/getSurvey`, this.state)
         .then(response => this.changePage(response))
         .catch(function (error){console.log(error)})
+
     }
 
-    changePage(res){
+    questionHandler(event){
+
+        let results = this.state.results
+
+        results[event.target.id] = {
+            question_id:event.target.name,
+            text:event.target.value
+        } 
+        
+        this.setState({
+                results:results
+                      })
+    }
+
+    // handles sending the survey to the backend
+    formHandler(event){
+        event.preventDefault()
+        const payload = {
+            user_id:this.state.student_id,
+            token:this.state.token,
+            form_id:this.state.form_id,
+            answers:this.state.results    
+        }
+        console.log(payload)
+        axios.post(`http://localhost:3001/api/submitSurvey`, payload)
+        .then(response => this.redirectOnSubmit(response))
+        .catch(function (error){console.log(error)})
+    }
+
+    redirectOnSubmit(res){
         if(res.status===200){
             this.setState({
-                questionList:res.data,
-                loading:false,
-                form_retreived:true
-            })
+              form_submitted:true
+            })  
             console.log(res.data)
         }
         else
             console.log("Something went wrong")
     }
-    // additional functions
-    // SubmitForm()
-    // handleChange2 for the answers to the survey questions
+
+    // processes the response from the form request
+    // stores the questions array in data
+    // changes form_retrieved to true so render can load the
+    // survey form to the page 
+    changePage(res){
+        if(res.status===200){
+            this.setState({
+                data:res.data.questions,
+                title:res.data.title,
+                type:res.data.type,
+                loading:false,
+                form_retreived:true
+            })
+            console.log(res.data.questions)
+        }
+        else
+            console.log("Something went wrong")
+    }
+
+    
 
     render(){
         //question array is just for testing right now has hardcoded values (see default props above)
-        const {questions} = this.props
-        const formStatus= this.state.form_retreived
-        
+       // const {questions} = this.props
+        const formStatus = this.state.form_retreived
+        const form_submitted = this.state.form_submitted
 
         if(!formStatus){
             return(
@@ -85,21 +130,35 @@ class Survey extends React.Component{
                 </form>
                 </div>
             )
-        } else {
+        } else if(formStatus){
+            
+            const title = this.state.title
+            var questions = Array.from(this.state.data);
             return(
                 <div>
-                    <h1 className="header">Survey1</h1>
-                    <form >
-                        <div>
-                        {questions.map((questions, i)=>
-                            <Question key={i} question={questions.question} />  
-                        )}
-                        <button>Submit</button>
-                        </div>
+                    <h1 className="header">{title} </h1>
+                    <form onSubmit={this.formHandler}>
+
+                       
+                        {questions.map((questions, i)=>  
+                           <div className="yellow" key={i+1}>
+                            <label name={"question" + i}>{i + ") " + questions.text}</label>
+                            <br></br>
+                            <input id={i} type="text" autoComplete="off" name={questions.id} value={this.state.results.value} onChange={this.questionHandler}/>
+                            <br></br>
+                            </div>
+                       )}
+                       
+                        <button type="submit"> Submit </button>
+                        
                     </form>
                 </div>
             )
-        } 
+        } else if (form_submitted) {
+            return(
+                <h1>Survey Completed</h1>
+            )
+        }
       
     }
 }
